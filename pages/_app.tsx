238686863d230254622aff8a9ade742a94352f63
BRM <beshoy.r.mansour@gmail.com>
2026-02-24 20:33:07 +0200

    update @vercel/speed-insights to version 1.3.1 and fix import path in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
